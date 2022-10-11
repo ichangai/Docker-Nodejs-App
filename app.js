d07721844833d8aa321be761a98182b9952afbee
ichangai gitundu <ichangaigitundu@gmail.com>
2022-10-11 14:09:14 +0300

    Added Mongoose and Docker IP address

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose
+  .connect("mongodb://ichangai:mypassword@172.30.0.2:27017/?authSource=admin", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
 const port = process.env.PORT || 3000;
 
@@ -10,6 +23,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
-
-
-
