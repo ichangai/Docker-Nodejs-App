82fdcfbdb97fd2c3f3de417a302d58a1a09824d0
ichangai gitundu <ichangaigitundu@gmail.com>
2022-10-14 06:57:53 +0300

    changes

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/users", userRoutes);
 
 app.get("/api/v1", (req, res) => {
-  res.send("We're now in Production. Digital Ocean");
+  res.send("We're now in Production. This is so awesome");
   console.log("Glorious Purpose"); 
 });
 
